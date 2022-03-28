513866b64261ed9a0e0fcbd935ad2a380f817325
Sagar <arorasagar1811@gmail.com>
2022-03-27 22:21:05 -0700

    initial commit for project-planner

---
+++ src/test/java/com/github/arorasagar/projectplanner/ProjectPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.arorasagar.projectplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
